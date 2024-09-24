8e3b8031a13b67833c96ca27bd64aa00a917855d
MateenCPH <cph-mr645@cphbusiness.dk>
2024-09-24 16:02:01 +0200

    update

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,7 +1,45 @@
 package dat;
 
+import io.javalin.Javalin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.javalin.config.JavalinConfig;
+
 public class Main {
+
+    private static Map<Integer, String> dogs = new HashMap<>();
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        dogs.put(1, "Golden Retriever");
+        dogs.put(2, "German Shepherd");
+        dogs.put(3, "Bulldog");
+        dogs.put(4, "Rottweiler");
+        dogs.put(5, "Pitbull");
+        dogs.put(6, "Mastiff");
+
+
+        var app = Javalin.create((config) -> {
+            config.router.contextPath = "/api/dog";
+            config.bundledPlugins.enableRouteOverview("/routes");
+        });
+
+        app.start(8080);
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/api/dog", ctx -> ctx.json(dogs, String.class));
+
+        app.post("/dogs", ctx -> {
+            String dog = ctx.body();
+            dogs.put(dog);
+            ctx.status(201);
+            ctx.result(dog);
+        });
+
+
     }
 }
---
+++ src/main/java/dat/dtos/DogDTO.java
@@ -0,0 +1,9 @@
+package dat.dtos;
+
+public class DogDTO {
+    int id;
+    String name;
+    String breed;
+    String gender;
+    int age;
+}
